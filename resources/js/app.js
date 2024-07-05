6f061f277b54cacf88a2b7a1e2b5bc3f599e644d
LivingtonMerello98 <livington.merello@gmail.com>
2024-07-06 00:10:36 +0200

    [upd] configurazione vite e bootsrap completata

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,12 @@
 import './bootstrap';
+
+//file scss default
+import '~resources/scss/app.scss';
+
+//file importato di bootsrap
+import * as bootstrap from 'bootstrap';
+
+//path x vite image manage
+import.meta.glob([
+    '../img/**'
+])
